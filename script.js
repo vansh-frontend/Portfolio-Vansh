33cf20ada6e3c3462a628f1e09c858c641c88f15
vansh-frontend <vanshdhalor04>
2024-05-22 10:32:03 +0530

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+// targer menu links class
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon"); 
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
